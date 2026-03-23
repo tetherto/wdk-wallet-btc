4c072ea7e531111e8a8793c76803dcd15dca8969
Tu Phan <tu.phan@tether.io>
2026-03-23 13:04:20 +0700

    fix: update jsdocs

--- types/src/wallet-account-read-only-btc.d.ts
+++ types/src/wallet-account-read-only-btc.d.ts
@@ -195,6 +195,9 @@ export type BtcWalletConfig = {
     network?: "bitcoin" | "regtest" | "testnet";
     /**
      * - The BIP address type used for key and address derivation.
+     *   - 44: [BIP-44 (P2PKH / legacy)](https://github.com/bitcoin/bips/blob/master/bip-0044.mediawiki)
+     *   - 84: [BIP-84 (P2WPKH / native SegWit)](https://github.com/bitcoin/bips/blob/master/bip-0084.mediawiki)
+     *   - Default: 84 (P2WPKH).
      */
     bip?: 44 | 84;
     /**
